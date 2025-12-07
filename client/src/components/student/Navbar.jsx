12eb2925984ffcab1798ad19937b9697b2535872
Prasanna Bhattarai <prasannabhattarai36@gmail.com>
2025-12-07 16:31:20 +0545

    signin

--- client/src/components/student/Navbar.jsx
+++ client/src/components/student/Navbar.jsx
@@ -1,9 +1,13 @@
 import React from 'react'
 import { assets } from '../../assets/assets'
 import { Link } from 'react-router-dom'
+import { useClerk , UserButton , useUser } from '@clerk/clerk-react'
 
 const Navbar = () => {
-  const isCourseListPage = location.pathname.includes('/course-list')
+  const isCourseListPage = location.pathname.includes('/course-list');
+  
+  const {openSignIn} = useClerk();
+  const {user}= useUser();
   
   return (
       <div className={`flex items-center justify-between 
@@ -17,7 +21,7 @@ const Navbar = () => {
           <button>Become Educator</button> |
           <Link to='/my-enrollments' >My Enrollments</Link>
         </div>
-        <button className='bg-blue-600 text-white px-5 py-2 rounded-full'>Create Account</button>
+        <button onClick={openSignIn} className='bg-blue-600 text-white px-5 py-2 rounded-full'>Create Account</button>
       </div>
       <div className='md:hidden flex items-center gap-2 sm:gap-5 text-gray-500'>
         <div>
