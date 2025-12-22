863b2f26b2cfff3decda62a0e16bf68b5bae9bed
Prasanna Bhattarai <prasannabhattarai36@gmail.com>
2025-12-22 15:03:01 +0545

    course

--- client/src/components/context/AppContext.jsx
+++ client/src/components/context/AppContext.jsx
@@ -1,10 +1,19 @@
-import { createContext } from "react";
+import { createContext, use } from "react";
+import { dummyCourses } from "../../assets/assets";
 
 export const AppContext = createContext();
 export const AppContextProvider = (props) =>{
-
-     const value ={
-
+const currency = import.meta.env.VITE_CURRENCY || '$';
+     
+        const [allCourses, setAllCourses] = useState([]);
+        const fetchAllCourses = async () => {
+            setAllCourses(dummyCourses)
+        }
+        useEffect(() => {
+            fetchAllCourses();
+        },[])
+        const value ={
+        currency , allCourses
         }
     return(
         
