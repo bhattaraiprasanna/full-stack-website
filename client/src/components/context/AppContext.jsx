02d10ae9ce291efcfd429e3417bf8b7a52b9dc83
Prasanna Bhattarai <prasannabhattarai36@gmail.com>
2025-12-24 18:59:01 +0545

    complete

--- client/src/components/context/AppContext.jsx
+++ client/src/components/context/AppContext.jsx
@@ -11,11 +11,24 @@ const navigate = useNavigate();
         const fetchAllCourses = async () => {
             setAllCourses(dummyCourses)
         }
+
+        //function to calculate average rating of the course
+        const calculateRating =(course)=>{
+           if (course.courseRatings.length ===0){
+            return 0;
+           }
+           let totalRating= 0;
+           course.courseRatings.forEach(rating => {
+            totalRating +=rating.rating;
+           })
+           return totalRating / course.courseRatings.length;
+        }
+
         useEffect(() => {
             fetchAllCourses();
         },[])
         const value ={
-        currency , allCourses , navigate
+        currency , allCourses , navigate , calculateRating
         }
     return(
         
