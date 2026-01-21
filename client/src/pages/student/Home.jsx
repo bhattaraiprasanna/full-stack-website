f039e2dbe9a01c96e67cdb3e01347267e87450a8
Prasanna Bhattarai <prasannabhattarai36@gmail.com>
2026-01-21 13:05:35 +0545

    testimonial

--- client/src/pages/student/Home.jsx
+++ client/src/pages/student/Home.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import Hero from '../../components/student/Hero'
 import Companies from '../../components/student/Companies'
 import CourseSection from '../../components/student/CourseSection'
+import TestimonialsSection from '../../components/student/TestimonialsSection'
 
 
 const Home = () => {
@@ -10,6 +11,7 @@ const Home = () => {
       <Hero />
       <Companies />
       <CourseSection />
+      <TestimonialsSection />
     </div>
   )
 }
